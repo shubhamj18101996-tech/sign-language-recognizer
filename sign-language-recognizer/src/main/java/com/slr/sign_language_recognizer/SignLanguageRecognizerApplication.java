855f0f6ec0bed4b374de962aac8f1812c0ed223b
Shubham <shubhamj18101996@gmail.com>
2026-08-15 21:05:32 +0530

    Initial commit

---
+++ sign-language-recognizer/src/main/java/com/slr/sign_language_recognizer/SignLanguageRecognizerApplication.java
@@ -0,0 +1,13 @@
+package com.slr.sign_language_recognizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignLanguageRecognizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignLanguageRecognizerApplication.class, args);
+	}
+
+}
